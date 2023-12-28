589c2775baf2ef5365633fc244a4664118eddcdf
ESvLana <Sv9toshka@mail.ru>
2023-12-28 19:18:39 +0300

    add Task_HM1_

---
+++ Task_HM1/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 91;
+int b = a % 7;
+int c = a % 13;
+
+if (b + c == 0)
+{
+    Console.WriteLine("Число " + a + " - кратно 7 и 13");
+}
+else
+{
+    Console.WriteLine("Число " + a + " - не кратно 7 и 13");
+}
