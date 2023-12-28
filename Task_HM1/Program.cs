c4adf5a1a2b1a4c4a274e24639798a8dc48d7fbb
ESvLana <Sv9toshka@mail.ru>
2023-12-28 19:44:17 +0300

    add Task_HM1_Right decision

--- Task_HM1/Program.cs
+++ Task_HM1/Program.cs
@@ -1,12 +1,34 @@
-﻿int a = 91;
+﻿int a = 148;
 int b = a % 7;
-int c = a % 13;
+int c = a % 23;
+
 
 if (b + c == 0)
 {
-    Console.WriteLine("Число " + a + " - кратно 7 и 13");
+    Console.WriteLine($"Число {a} - кратно 7 и 23");
 }
 else
 {
-    Console.WriteLine("Число " + a + " - не кратно 7 и 13");
+    Console.WriteLine($"Число {a} - не кратно 7 и 23");
 }
+
+
+// using System;
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.Write("Введите число: ");
+//         int a = Convert.ToInt32(Console.ReadLine());
+
+
+//         if (a % 7 == 0 && a % 23 == 0)
+//         {
+//             Console.WriteLine($"Число {a} - кратно 7 и 23");
+//         }
+//         else
+//         {
+//             Console.WriteLine($"Число {a} - не кратно 7 и 23");
+//         }
+//     }
+// }
