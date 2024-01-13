585099833fc311f1df8992e034380aceb807859e
ESvLana <Sv9toshka@mail.ru>
2024-01-13 17:03:29 +0300

    Added code Task3

--- Task_HM3/Program.cs
+++ Task_HM3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число Х (от 10 до 99): ");
+        int X = Convert.ToInt32(Console.ReadLine());
+        int Digit = X / 10;
+        int Remains = X % 10;
+
+        
+        if (Digit > Remains)
+        {
+            Console.WriteLine(Digit);
+        }
+        else
+        {
+            Console.WriteLine(Remains);
+        }
+    }
+}
