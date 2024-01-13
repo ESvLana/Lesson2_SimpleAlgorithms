cb32dc596878365fa6274b9c9fd892c225641f74
ESvLana <Sv9toshka@mail.ru>
2024-01-13 15:13:22 +0300

    Added code Task2

---
+++ Task_HM2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите X: ");
+        int X = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите Y: ");
+        int Y = Convert.ToInt32(Console.ReadLine());
+
+
+        if (X > 0 && Y > 0)
+        {
+            Console.WriteLine($"Точка с координатами ({X};{Y}) находится в первой координатной четверти плоскости");
+        }
+        if (X < 0 && Y > 0)
+        {
+            Console.WriteLine($"Точка с координатами ({X};{Y}) находится во ыторой координатной четверти плоскости");
+        }
+        if (X < 0 && Y < 0)
+        {
+            Console.WriteLine($"Точка с координатами ({X};{Y}) находится в третьей координатной четверти плоскости");
+        }
+        if (X > 0 && Y < 0)
+        {
+            Console.WriteLine($"Точка с координатами ({X};{Y}) находится в четвертой координатной четверти плоскости");
+        }
+    }
+}
